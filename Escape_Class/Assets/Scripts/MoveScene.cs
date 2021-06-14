abb9e198466d4dfd483aec8ca786806985771eef
Theodoros Alexandrou <theodoro-s@hotmail.com>
2021-06-14 14:25:00 +0300

    Add trigger object

---
+++ Escape_Class/Assets/Scripts/MoveScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    [SerializeField] private string SampleScene;
+    [SerializeField] private GameObject uiElement;
+    private void OnTriggerStay(Collider other)
+    {
+
+        if(other.CompareTag("Player"))
+        {
+            //Make UI Appear
+            uiElement.SetActive(true);
+            //Button Press
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(SampleScene);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            uiElement.SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

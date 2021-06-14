752a6351ae23300f68ca819ceaf9664318736cad
Theodoros Alexandrou <theodoro-s@hotmail.com>
2021-06-14 16:13:56 +0300

    Change Scene when Player Trigger to the object

--- Escape_Class/Assets/Scripts/ChangeScene.cs
+++ Escape_Class/Assets/Scripts/ChangeScene.cs
@@ -5,8 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+
+    [SerializeField] private GameObject uiElement;
+    [System.Obsolete]
+    
+    bool colliding;
+
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("Stage02");
+        uiElement.SetActive(true);
+        colliding = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        uiElement.SetActive(false);
+        colliding = false;
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    //SceneManager.LoadScene("Stage02");
+
+    //}
+
+    [System.Obsolete]
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    Application.LoadLevel("Stage02");
+        //}
+        if (colliding && Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel("Stage02");
+        }
     }
 }

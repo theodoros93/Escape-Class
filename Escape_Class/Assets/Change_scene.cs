808a1ec70d0567da591b064698b8daee7ad36b7b
Theodoros Alexandrou <theodoro-s@hotmail.com>
2021-06-17 10:00:32 +0300

    Add Start page with buttons

---
+++ Escape_Class/Assets/Change_scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_scene : MonoBehaviour
+{
+    public void btn_change_scene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}

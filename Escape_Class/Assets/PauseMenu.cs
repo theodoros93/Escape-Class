d1784926d6c2b2eeb83229af2e289e6a44d76883
Christos <boutsikas@tessera.gr>
2021-06-27 01:38:19 +0300

    Add pause menu on main game

---
+++ Escape_Class/Assets/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("UpdateCalled!");
+            if (gameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        Debug.Log("ResumeCalled!");
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause() {
+        Debug.Log("PauseCalled!");
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
---
+++ Escape_Class/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void onPause() 
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame() 
+    {
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame() 
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start page");
+    }
+}
